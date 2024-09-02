f1790200d73689683fa5e163722938fc464f53d6
Lucas Cipriano Lima <lucasc.lima26@gmail.com>
2024-09-02 19:12:09 -0300

    use routes on entry file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
+import { authRoute } from "./routes/v1/auth.route";
+import { productsRoute } from "./routes/v1/products.route";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .use(productsRoute)
+  .use(authRoute)
+  .listen(3000);
+
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
